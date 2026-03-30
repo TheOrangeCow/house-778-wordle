2d6b2bd24daeff8315bf6e3631f2fa986b1ef402
TheOrangeCow <138312355+TheOrangeCow@users.noreply.github.com>
2026-03-30 15:31:46 +0100

    update

--- username.php
+++ username.php
@@ -1,8 +1,16 @@
 <?php
 
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'domain' => '.theorangecow.org', 
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Lax'
+]);
 session_start();
 
-header('Access-Control-Allow-Origin: https://subdomain.house-778.theorangecow.org');
+header('Access-Control-Allow-Origin: https://wordle.house-778.theorangecow.org');
 header('Access-Control-Allow-Credentials: true');
 header('Content-Type: application/json');
 
